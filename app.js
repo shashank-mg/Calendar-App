487f9bce84bb7b954c4b6feffac3487800cc8165
shashank m.g <52480486+shashank-mg@users.noreply.github.com>
2020-12-26 16:21:04 +0530

    back button feature in build

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 class Calendar {
   constructor() {
     this.flag = 0;
-    this.last = 0;
-    this.first = 0;
     this.monthRecord = 0;
+    this.yearRecord = 0;
   }
 
   startCalculate() {
+    // console.log(this.start);
     if (this.start < 34) this.start -= 27;
-    else if (this.start >= 34) this.start = this.start - 35;
+    else if (this.start >= 34) this.start -= 34;
   }
 
   caller() {
     let start = new Date();
     this.days(start.getDay());
-    this.dates(start.getDate(), start.getDay(), start.getMonth());
-    this.months(start.getMonth());
-    this.years(start.getFullYear());
+    this.months(this.monthRecord);
+    this.dates(start.getDate(), start.getDay(), this.monthRecord);
+    this.years(this.yearRecord);
   }
 
   commonCall = () => {
     if (this.flag === 0) {
       let start = new Date();
+      this.monthRecord = start.getMonth();
+      this.yearRecord = start.getFullYear();
       this.days(start.getDay());
       this.dates(start.getDate(), start.getDay(), start.getMonth());
       this.months(start.getMonth());
@@ -30,18 +32,33 @@ class Calendar {
   };
 
   days = (dy) => {
-    // console.group(days[dy]);
+    // console.group(all_days[dy]);
   };
 
   months = (m) => {
-    month_display.textContent = months[m][0];
+    if (m > 11) {
+      this.monthRecord = 0;
+      this.yearRecord++;
+    }
+    if (m < 0) {
+      this.monthRecord = 11;
+      this.yearRecord--;
+    }
+    month_display.textContent = months[this.monthRecord][0];
   };
 
-  years = (y) => {
-    year_display.textContent = y;
-  };
+  years = (y) => (year_display.textContent = y);
 
-  dates = (dt, dy, m) => {
+  clear(m) {
+    for (let i = 0; i <= 36; i++) {
+      all_dates[i].textContent = "";
+    }
+    if (months[m][0] === "February") {
+      months[m][1] = this.yearRecord % 4 !== 0 ? 28 : 29;
+    }
+  }
+
+  firstOfMonth = (dy, dt) => {
     let day_1;
     let map_day = new Map();
     let currentday = all_days.indexOf(all_days[dy]);
@@ -56,44 +73,66 @@ class Calendar {
         map_day.set(i, all_days[currentday--]);
       }
     }
+
     let starter = map_day.get(1);
     let begin = all_days.indexOf(starter);
+    return begin;
+  };
 
+  dates = (dt, dy, m) => {
     if (this.flag === 0) {
+      let begin = this.firstOfMonth(dy, dt);
+      this.starting = begin;
+      this.clear(m);
       for (var j = 1; j <= dt; j++) {
         all_dates[begin++].textContent = j;
       }
-      all_dates[begin - 1].style.backgroundColor = "teal";
+      this.store = begin - 1;
+      all_dates[begin - 1].style.backgroundColor = "crimson";
       for (let i = j; i <= months[m][1]; i++) {
         all_dates[begin++].textContent = i;
       }
+      console.log(this.starting);
       this.start = begin - 1;
-    }
-
-    if (this.flag > 0) {
-      this.startCalculate();
-      for (let i = this.start; i >= 0; i--) {
-        all_dates[i].textContent = "";
+    } else {
+      this.clear(m);
+      all_dates[this.store].style.backgroundColor = null;
+      if (this.datesChange) {
+        this.startCalculate();
+        this.starting = this.start;
+        console.log(this.starting);
+        for (let j = 1; j <= months[this.monthRecord][1]; j++) {
+          all_dates[this.start++].textContent = j;
+        }
+        this.start--;
+      } else {
+        this.clear(m);
+        console.log(this.starting, months[this.monthRecord]);
+        let endAt = this.starting - 1 + months[this.monthRecord][1] + 4;
+        console.log(endAt);
+        for (let i = months[this.monthRecord][1]; i >= 1; i--) {
+          all_dates[endAt--].textContent = i;
+        }
       }
-      for (let j = 1; j <= 31; j++) {
-        all_dates[this.start++].textContent = j;
-      }
-      console.log(this.start);
     }
   };
 
   keepRunning = () => {
-    setInterval(() => this.commonCall(), 500);
+    setInterval(() => this.commonCall(), 5000);
     this.commonCall();
   };
 
   forward = () => {
     this.flag++;
+    this.monthRecord++;
+    this.datesChange = 1;
     this.caller();
   };
 
   backward = () => {
     this.flag--;
+    this.monthRecord--;
+    this.datesChange = 0;
     this.caller();
   };
 }
--- elements.js
+++ elements.js
@@ -4,20 +4,20 @@ let all_dates = document.querySelector(".dates").children;
 let left_arrow = document.querySelector(".left-arrow");
 let right_arrow = document.querySelector(".right-arrow");
 
-let months = {
-  0: ["January", 31],
-  1: ["February", 29],
-  2: ["March", 31],
-  3: ["April", 30],
-  4: ["May", 31],
-  5: ["June", 30],
-  6: ["July", 31],
-  7: ["August", 31],
-  8: ["September", 30],
-  9: ["October", 31],
-  10: ["November", 30],
-  11: ["December", 31],
-};
+let months = [
+  ["January", 31],
+  ["February", 29],
+  ["March", 31],
+  ["April", 30],
+  ["May", 31],
+  ["June", 30],
+  ["July", 31],
+  ["August", 31],
+  ["September", 30],
+  ["October", 31],
+  ["November", 30],
+  ["December", 31],
+];
 
 let all_days = [
   "Sunday",
